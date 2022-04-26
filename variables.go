1c61bb4a65d075223f9dfc490baea48c99c2de5f
Faisal Ali <fzl.ali33@gmail.com>
2022-04-26 23:01:48 +0530

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var distToMars int64
+	var gravity float32 = 9.8
+	var isMusicOver bool = true
+
+	distToMars = 6150000
+
+	fmt.Println(distToMars)
+	fmt.Println(gravity)
+	fmt.Println(isMusicOver)
+
+}
