4f7d181fc03aed96d70b33eb9738c5bf626779d4
Faisal Ali <fzl.ali33@gmail.com>
2022-04-26 23:02:02 +0530

    Create strings.go

---
+++ strings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nameOfSong string = "Stop Stop"
+	var artistName string = "The Julie Ruin"
+	var description string
+
+	description = nameOfSong + " is sung by " + artistName
+
+	fmt.Println(description)
+}
